61b06f586a4126637520eb0e6c40b6b4f2710fa7
Maksym Puzanov <puzanov.maksym@gmail.com>
2024-11-03 04:06:35 +0200

    responsive profile page

--- src/components/pages/Profile/ProfileFormWrapper/ProfileFormWrapper.tsx
+++ src/components/pages/Profile/ProfileFormWrapper/ProfileFormWrapper.tsx
@@ -1,5 +1,5 @@
 import Button from '@/components/ui/buttons/Button/Button'
-import type { DetailedHTMLProps, FC, FormHTMLAttributes, ReactElement, ReactNode } from 'react'
+import type { DetailedHTMLProps, FC, FormEvent, FormHTMLAttributes, ReactElement, ReactNode } from 'react'
 import '../Profile.scss'
 
 interface PropsType extends DetailedHTMLProps<FormHTMLAttributes<HTMLFormElement>, HTMLFormElement> {
@@ -8,11 +8,12 @@ interface PropsType extends DetailedHTMLProps<FormHTMLAttributes<HTMLFormElement
   children?: ReactNode
   button?: ReactElement<HTMLButtonElement>
   handleSave?: () => void
+  onSubmit?: (e: FormEvent<HTMLFormElement>) => void
 }
 
-const ProfileFormWrapper: FC<PropsType> = ({ title, text, children, button, handleSave }) => {
+const ProfileFormWrapper: FC<PropsType> = ({ title, text, children, button, onSubmit }) => {
   return (
-    <form className='profile-form-wrapper'>
+    <form className='profile-form-wrapper' onSubmit={onSubmit}>
       <div className='profile-form-wrapper__head'>
         <h1 className='profile-form-wrapper__title'>{title}</h1>
         <p className='profile-form-wrapper__text'>{text}</p>
@@ -23,7 +24,9 @@ const ProfileFormWrapper: FC<PropsType> = ({ title, text, children, button, hand
       <div className='profile-form-wrapper__body'>{children}</div>
 
       <div className='profile-form-wrapper__footer'>
-        <Button onClick={handleSave}>Save</Button>
+        <Button type='submit' className='profile-form-wrapper__save-btn'>
+          Save
+        </Button>
       </div>
     </form>
   )
