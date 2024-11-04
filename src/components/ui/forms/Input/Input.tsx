bf49ceb7b3be643e251004cfffc7d939e6563a10
Maksym Puzanov <puzanov.maksym@gmail.com>
2024-11-04 10:23:25 +0200

    add error status to input

--- src/components/ui/forms/Input/Input.tsx
+++ src/components/ui/forms/Input/Input.tsx
@@ -1,13 +1,20 @@
 import type { DetailedHTMLProps, FC, InputHTMLAttributes } from 'react'
 import './Input.scss'
 
-interface PropsType extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-
+interface PropsType
+  extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  isError?: boolean
+  isDisable?: boolean
   className?: string
 }
 
-const Input: FC<PropsType> = ({ className, ...props }) => {
-  return <input className='input' {...props} />
+const Input: FC<PropsType> = ({ className, isError, isDisable, ...props }) => {
+  return (
+    <input
+      className={`input ${isDisable ? 'is-disable' : ''} ${isError ? 'is-error' : ''}`}
+      {...props}
+    />
+  )
 }
 
 export default Input
